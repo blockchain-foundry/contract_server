f49319f495fd40e9473da755fb3515f6a44b9843
LSChyi <alan81920@gmail.com>
2016-08-08 10:16:08 +0800

    add oracles list API in contract server

--- contract_server/oracles/views.py
+++ contract_server/oracles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from oracles.models import Oracle
+from oracles.serializers import OracleSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
-# Create your views here.
+class OracleList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Oracle.objects.all()
+    serializer_class = OracleSerializer
+    
+    def get(self, request):
+        return self.list(request)
