db775c520b192d28a98116130d2170ca111b3809
peterjune <peter88461@gmail.com>
2016-08-23 11:48:33 +0800

    2016/08/23 add sha3, modify models

--- contract_server/oracles/serializers.py
+++ contract_server/oracles/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from oracles.models import Oracle
+from oracles.models import *
 
 class OracleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Oracle
-        fields = ('created', 'name', 'url')
+	class Meta:
+		model = Oracle
+		fields = ('created', 'name', 'url')
+
+class ContractSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Contract
+		fields = ('created', 'source_code', 'multisig_address', 'interface', 'oracles')
+
