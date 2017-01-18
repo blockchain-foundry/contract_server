df49bee889a43bff6e50c23cfaadbf056dc8e52c
Chun Wei, Chang <m04120310@gmail.com>
2017-01-18 13:38:50 +0800

    Change json to form data.

--- contract_server/contracts/urls.py
+++ contract_server/contracts/urls.py
@@ -5,11 +5,11 @@ from contracts import views
 
 urlpatterns = [
     url(r'^contracts/$', views.Contracts.as_view()),
-    url(r'^contracts/(?P<multisig_address>[A-Za-z0-9]+)/', views.ContractFunc.as_view()),
+    url(r'^contracts/(?P<multisig_address>[A-Za-z0-9]+)/', views.ContractUsage.as_view()),
     url(r'^contractlist/$', views.ContractList.as_view()),
    # url(r'^functions/$', views.transfer_money_to_account),
     url(r'^execute/$', views.create_multisig_payment),
-    url(r'^withdraw/$', views.withdraw_from_contract),
+    url(r'^withdraw/$', views.WithdrawFromContract.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
