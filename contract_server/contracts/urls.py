5b1c6fa37efc8901c58173c2400667231bc03fbf
icence <icence.lee@gmail.com>
2017-02-23 17:33:40 +0800

    add $ to url

--- contract_server/contracts/urls.py
+++ contract_server/contracts/urls.py
@@ -6,8 +6,8 @@ from contracts import views
 urlpatterns = [
     url(r'^contracts/$', views.Contracts.as_view()),
     url(r'^subcontracts/(?P<multisig_address>[A-Za-z0-9]+)/$', views.SubContracts.as_view()),
-    url(r'^subcontracts/(?P<multisig_address>[A-Za-z0-9]+)/(?P<deploy_address>[0-9]+)', views.SubContractFunc.as_view()),
-    url(r'^contracts/(?P<multisig_address>[A-Za-z0-9]+)/', views.ContractFunc.as_view()),
+    url(r'^subcontracts/(?P<multisig_address>[A-Za-z0-9]+)/(?P<deploy_address>[0-9]+)/$', views.SubContractFunc.as_view()),
+    url(r'^contracts/(?P<multisig_address>[A-Za-z0-9]+)/$', views.ContractFunc.as_view()),
     url(r'^contractlist/$', views.ContractList.as_view()),
     url(r'^execute/$', views.create_multisig_payment),
     url(r'^withdraw/$', views.WithdrawFromContract.as_view()),
