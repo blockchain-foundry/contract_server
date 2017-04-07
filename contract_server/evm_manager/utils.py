76c3225007247aff64a4541ea30271435160b713
icence <icence.lee@gmail.com>
2017-04-07 09:09:55 +0000

    [refactor] new func to process tx

--- contract_server/evm_manager/utils.py
+++ contract_server/evm_manager/utils.py
@@ -98,3 +98,53 @@ def get_nonce(multisig_address, sender_address):
         content = json.load(f)
         if sender_evm_address in content['accounts']:
             return content['accounts'][sender_evm_address]['nonce']
+
+
+def process_tx(tx):
+    tx_hash = tx.get('txid') or tx.get('hash')
+    typ = tx.get('type')
+    time = int(tx.get('time'))
+    vins = _process_vins(tx)
+    vouts = _process_vouts(tx)
+    op_return = None if typ == 'NORMAL' else _process_op_return(tx)
+    return tx_hash, typ, time, vins, vouts, op_return
+
+
+def _process_vouts(tx):
+    vouts = tx.get('vout') or tx.get('vouts')
+    for vout in vouts:
+        vout['address'] = vout.get('scriptPubKey').get('addresses') or vout.get('address') or ''
+        vout['address'] = vout['address'] if isinstance(vout['address'], str) else vout['address'][0]
+        vout['amount'] = int(vout.get('value') or vout.get('amount'))
+        vout['color'] = int(vout['color'])
+        vout['n'] = int(vout['n'])
+        del vout['scriptPubKey']
+        try:
+            del vout['value']
+        except:
+            pass
+    return vouts
+
+
+def _process_vins(tx):
+    vins = tx.get('vin') or tx.get('vins')
+    for vin in vins:
+        vin['vout'] = int(vin.get['vout'])
+        vin['tx_hash'] = vin.get('txid') or vin.get('tx_id')
+        delete_field = ['txid', 'tx_id', 'address', 'color', 'amount', 'scriptSig', 'sequence']
+        for field in delete_field:
+            try:
+                del vin[field]
+            except:
+                pass
+    return vins
+
+
+def _process_op_return(tx):
+    vouts = tx.get('vout') or tx.get('vouts')
+    for vout in vouts:
+        if int(vout['color']) == 0:
+            if isinstance(vout['scriptPubkey'], str):
+                return vout['scriptPubkey'][8:]
+            else:
+                return vout['scriptPubkey']['asm'][10:]
