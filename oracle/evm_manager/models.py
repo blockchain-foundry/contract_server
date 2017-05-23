4b366b4a5efdea644dada637a963f9e1a7642fae
陳柏翰 <chenbohan@chinkashiwas-MacBook-Air.local>
2017-05-23 14:53:52 +0800

    Remove redundant folders and modified .travis.yml

--- oracle/evm_manager/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class StateInfo(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    multisig_address = models.CharField(max_length=100, primary_key=True)
-    latest_tx_time = models.CharField(max_length=100, blank=True, default='')
-    latest_tx_hash = models.CharField(max_length=100, blank=True, default='')
